aaef5ff6bec3c4be2091686a79fc9765ffab7f01
kimhyeok <kimmang940715@naver.com>
2023-09-25 10:54:02 +0900

    리소스 임포트

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    public float fTimeLimit = 1f;
+
+    private float m_fTimeLeft = 0f;
+
+    private void Awake()
+    {
+        m_fTimeLeft = fTimeLimit;
+    }
+
+    private void Update()
+    {
+        m_fTimeLeft -= Time.deltaTime;
+        if (m_fTimeLeft < 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
